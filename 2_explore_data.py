e77e3a9918c68ae60da4c17a0aef918cc4261103
sishirsubedi <sishirsubedi@tamu.edu>
2017-11-12 00:34:51 -0600

    new files

--- 2_explore_data.py
+++ 2_explore_data.py
@@ -5,9 +5,19 @@ import random
 import numpy as np
 import math
 import collections
-
+import seaborn as sns
 
 
 df_all_data = pd.read_csv("df_icu_admission_combine.csv",sep=',',header=0)#,index_col=0)
 len(df_all_data)
 df_all_data.head(2)
+
+df_xdata = df_all_data.loc[:,'age':'urine_max']
+df_n_xdata= (df_xdata-df_xdata.mean())/df_xdata.std()
+df_n_xdata.mean()
+
+# calculate the correlation matrix
+corr = df_xdata.corr()
+
+# plot the heatmap
+sns.heatmap(corr,xticklabels=corr.columns,yticklabels=corr.columns)
