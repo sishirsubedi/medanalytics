dd98ff2907603aa68802de11611335ea0862a078
sishirsubedi <sishirsubedi@tamu.edu>
2017-11-24 14:47:05 -0600

    rbf

--- 4_rbf_lm.py
+++ 4_rbf_lm.py
@@ -61,13 +61,13 @@ logr_ranking = pd.DataFrame(logr_ranking,columns=['features','logr'])
 logr_ranking.sort_values('logr',inplace=True)
 
 
-todrop = logr_ranking.iloc[10: ,0]
+todrop = logr_ranking.iloc[4: ,0]
 df_all_data.drop(todrop, axis=1, inplace=True)
 df_all_data.shape
 
 len(df_all_data)
 df_all_data.head(2)
-df_xdata = df_all_data.loc[:,'urea_n_min':'51250_var']
+df_xdata = df_all_data.loc[:,'urea_n_min':'51006_min']
 df_n_xdata= (df_xdata-df_xdata.mean())/df_xdata.std()
 df_n_xdata.mean()
 df_n_xdata.head(20)
@@ -89,8 +89,6 @@ y_test = pd.DataFrame(y_test)
 X_train.shape
 y_train.shape
 
-X= X_train
-y = y_train.values
 
 ####################################################
 
@@ -100,76 +98,44 @@ import statsmodels.api as sm
 import  matplotlib.pylab as plt
 from sklearn.cluster import KMeans,AgglomerativeClustering
 
-def rbf(x, c, s):
-    return np.exp(-1 / (2 * s**2) * (x-c)**2)
+import math
+
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))
 
 
-def kmeans(X, k):
-    """Performs k-means clustering for 1D input
+def rbf(x, mu, s):
 
-    Arguments:
-        X {ndarray} -- A Mx1 array of inputs
-        k {int} -- Number of clusters
+    var = sum(s)/len(s)
 
-    Returns:
-        ndarray -- A kx1 array of final cluster centers
-    """
-
-    # randomly select initial clusters from input data
-    clusters = np.random.choice(np.squeeze(X), size=k)
-    prevClusters = clusters.copy()
-    stds = np.zeros(k)
-    converged = False
-
-    while not converged:
-        """
-        compute distances for each cluster center to each point 
-        where (distances[i, j] represents the distance between the ith point and jth cluster)
-        """
-        distances = np.squeeze(np.abs(X[:, np.newaxis] - clusters[np.newaxis, :]))
-
-        # find the cluster that's closest to each point
-        closestCluster = np.argmin(distances, axis=1)
-
-        # update clusters by taking the mean of all of the points assigned to that cluster
-        for i in range(k):
-            pointsForCluster = X[closestCluster == i]
-            if len(pointsForCluster) > 0:
-                clusters[i] = np.mean(pointsForCluster, axis=0)
-
-        # converge if clusters haven't moved
-        converged = np.linalg.norm(clusters - prevClusters) < 1e-6
-        prevClusters = clusters.copy()
-
-    distances = np.squeeze(np.abs(X[:, np.newaxis] - clusters[np.newaxis, :]))
-    closestCluster = np.argmin(distances, axis=1)
-
-    clustersWithNoPoints = []
-    for i in range(k):
-        pointsForCluster = X[closestCluster == i]
-        if len(pointsForCluster) < 2:
-            # keep track of clusters with no points or 1 point
-            clustersWithNoPoints.append(i)
-            continue
-        else:
-            stds[i] = np.std(X[closestCluster == i])
-
-    # if there are clusters with 0 or 1 points, take the mean std of the other clusters
-    if len(clustersWithNoPoints) > 0:
-        pointsToAverage = []
-        for i in range(k):
-            if i not in clustersWithNoPoints:
-                pointsToAverage.append(X[closestCluster == i])
-        pointsToAverage = np.concatenate(pointsToAverage).ravel()
-        stds[clustersWithNoPoints] = np.mean(np.std(pointsToAverage))
-
-    return clusters, stds
+    return np.exp(-1 / (2 * var) * np.sum((x-mu)**2))
 
 
+def kmeans(xdata, k):
+
+    clust_num = k
+    # kmeans = KMeans(n_clusters=clust_num, random_state=0).fit(xdata)
+    hc = AgglomerativeClustering(n_clusters=clust_num).fit(xdata)
+    xdata= pd.DataFrame(xdata)
+    xdata['clust'] = hc.labels_
+    xdata.head(20)
+
+    centers =[]
+    stds =[]
+
+    for c in range(k):
+        xdata_1 = xdata.loc[xdata['clust'] == c]
+        centers.append(xdata_1.iloc[:, 0:xdata.shape[1]-1].mean())
+        stds.append(xdata_1.iloc[:, 0:xdata.shape[1] - 1].var())
+
+    return centers, stds
+
+
+############# learning
 class RBFNet(object):
     """Implementation of a Radial Basis Function Network"""
 
-    def __init__(self, k=2, lr=0.01, epochs=100, rbf=rbf, inferStds=True):
+    def __init__(self, k=2, lr=0.001, epochs=1000, rbf=rbf, inferStds=True):
         self.k = k
         self.lr = lr
         self.epochs = epochs
@@ -193,42 +159,123 @@ class RBFNet(object):
         for epoch in range(self.epochs):
             for i in range(X.shape[0]):
                 # forward pass
-                a = np.array([self.rbf(X[i], c, s) for c, s, in zip(self.centers, self.stds)])
+                a=[]
+                for cen in range(len(self.centers)):
+                    a.append(rbf(X[i], self.centers[cen].values, self.stds[cen].values))
+                a = np.array(a)
+
                 F = a.T.dot(self.w) + self.b
 
-                loss = (y[i] - F).flatten() ** 2
+                #yi = np.array([0.5 if y[i] == 0.0 else 1.0])
+                loss = -(y[0] - F) .flatten()
                 #print('Loss: {0:.2f}'.format(loss[0]))
 
                 # backward pass
-                error = -(y[i] - F).flatten()
+                # error = -(y[i] - F).flatten()
 
                 # online update
-                self.w = self.w - self.lr * a * error
-                self.b = self.b - self.lr * error
+                self.w = self.w - self.lr * a * loss[0]
+                self.b = self.b - self.lr * loss[0]
+            print self.w, self.b
 
     def predict(self, X):
         y_pred = []
+
         for i in range(X.shape[0]):
-            a = np.array([self.rbf(X[i], c, s) for c, s, in zip(self.centers, self.stds)])
+            # forward pass
+            a=[]
+            for cen in range(len(self.centers)):
+                a.append(rbf(X[i], self.centers[cen].values, self.stds[cen].values))
+            a = np.array(a)
+
             F = a.T.dot(self.w) + self.b
-            y_pred.append(F)
+            yi = np.array([0.0 if sigmoid(F) < 0.5 else 1.0])
+            y_pred.append(yi)
+
+
         return np.array(y_pred)
 
     def coefs(self):
         return self.w
 
 
-# sample inputs and add noise
 
-mixture = []
 
-for i in range(X_train.shape[1]):
-    rbfnet = RBFNet(lr=1e-2, k=10)
-    X= X_train.iloc[:,i].values
-    rbfnet.fit(X, y)
-    coefs = rbfnet.coefs()
-    mixture.append(coefs)
 
-df_hidden_xdata = pd.DataFrame(mixture)
 
-df_hidden_xdata.shape
+
+rbfnet = RBFNet(lr=1e-2, k=2,epochs=1000)
+X= X_train.values
+print X.shape
+y = y_train.values
+rbfnet.fit(X, y)
+yhat = rbfnet.predict(X)
+
+match = 0.0
+for m in range(0, len(yhat)):
+    if yhat[m] == y[m]: match += 1
+
+match = match / len(yhat)
+
+print match
+
+
+
+ ############### use LM for optimization
+
+class RBFNet_LM(object):
+    """Implementation of a Radial Basis Function Network"""
+
+    def __init__(self, k=2, rbf=rbf, inferStds=True):
+        self.k = k
+        self.rbf = rbf
+        self.inferStds = inferStds
+
+
+    def fit(self, X, y):
+        if self.inferStds:
+            # compute stds from data
+            self.centers, self.stds = kmeans(X, self.k)
+        else:
+            # use a fixed std
+            self.centers, _ = kmeans(X, self.k)
+            dMax = max([np.abs(c1 - c2) for c1 in self.centers for c2 in self.centers])
+            self.stds = np.repeat(dMax / np.sqrt(2*self.k), self.k)
+
+        # training
+
+        xmat = []
+        for i in range(X.shape[0]):
+            # forward pass
+            a=[]
+            for cen in range(len(self.centers)):
+                a.append(rbf(X[i], self.centers[cen].values, self.stds[cen].values))
+            temp =[1.0]
+
+            for x in a : temp.append(x)
+            xmat.append(temp)
+            print temp
+        return xmat
+
+
+rbfnet = RBFNet_LM(k=2)
+X= X_train.values
+print X.shape
+y = y_train.values
+xmat = rbfnet.fit(X, y)
+
+xmat = np.array(xmat)
+
+logit = sm.Logit(y_train, xmat)
+results = logit.fit()
+
+yhat = pd.DataFrame(results.predict(xmat), columns=['predict'])
+
+yhat['predict'] = yhat['predict'].apply(lambda x: 0.0 if x < 0.5 else 1.0)
+
+match = 0.0
+for m in range(0, len(yhat)):
+    if yhat.iloc[m].values == y_train.iloc[m].values: match += 1
+
+match = match / len(yhat)
+print match
